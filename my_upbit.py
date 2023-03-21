ea217cb3daaf6dcca33b763a9de5c1854c987202
고이아스 <42239732+dickss@users.noreply.github.com>
2023-03-21 15:30:02 +0900

    결과를 file에 기록하는 코드 추가함

--- my_upbit.py
+++ my_upbit.py
@@ -1,20 +1,38 @@
+import datetime
+# import threading
 from my_upbit_class import Myupbit
 
 access_key = "NKKTfqpYqMKYWbbwJSkn5rEQPuJg35ExOr8Hpst2"
 secret_key = "2vhZZI6h9yP4vzBLJa2cMlx1UJgMmj2u7dKBRCun"
 goias_upbit = Myupbit(access_key, secret_key)
 
-while True:
-    goias_upbit.watch_and_order_many("BTC/KRW", 6000, 0.5)
-    goias_upbit.watch_and_order_many("DOGE/KRW", 6000, 0.5)
-    goias_upbit.watch_and_order_many("XRP/KRW", 6000, 0.5)
-    
-    # infos = goias_upbit.fetch_ticker("BTC/KRW")    # 
+# 파일명 생성
+now = datetime.datetime.now()
+file_name = now.strftime("%Y%m%d_%H%M%S.txt")
 
-    # goias_upbit.watch_and_order("BTC/KRW", 6000, 0.5)
-    # goias_upbit.watch_and_order("DOGE/KRW", 6000, 0.5)
-    # goias_upbit.watch_and_order("XRP/KRW", 6000, 0.5)
+# 파일 생성 및 Open
+with open(file_name, "a") as f:
+    # 로그파일의 Header 작성
+    title_txt = "buy_sell,date_time,symbol,전일종가,현재가,전일대비(%),등락,주문금액"
+    f.write(title_txt + "\n")   
 
+    # 루프를 돌림
+    while True:
+        goias_upbit.watch_and_order_many(f, "BTC/KRW", 6000, 0.5)   # f : 파일패스 및 파일명을 던져준다
+        goias_upbit.watch_and_order_many(f, "DOGE/KRW", 6000, 0.5)
+        goias_upbit.watch_and_order_many(f, "XRP/KRW", 6000, 0.5)
+        
+        # if not thread.is_alive():    
+        #     break
+        #     # infos = goias_upbit.fetch_ticker("BTC/KRW")    # 
+
+        # goias_upbit.watch_and_order("BTC/KRW", 6000, 0.5)
+        # goias_upbit.watch_and_order("DOGE/KRW", 6000, 0.5)
+        # goias_upbit.watch_and_order("XRP/KRW", 6000, 0.5)
+
+    # 로그 파일 내용 확인
+    with open(file_name, "r") as f:
+        print(f.read())
 
 # import ccxt
 
--- my_upbit_class.py
+++ my_upbit_class.py
@@ -1,5 +1,7 @@
 import ccxt
+import datetime
 import pandas as pd
+import keyboard
 
 #####################################
 # Class 선언
@@ -13,10 +15,16 @@ class Myupbit():
         self.upbit.apiKey = access
         self.upbit.secret = secret
         self.is_buy_count = False
-        # self.is_buy = False
-    
+        # self.is_buy = False    
+
+    # 특정키(F4)가 눌리면 while loop에서 빠져나오는 메소드
+    def check_f4_pressed(self):
+        while True:
+            if keyboard.is_pressed('ctrl+c'):
+                break
+
     # (함수 == 메소드) 다건 가상화폐 가격 감시 및 주문
-    def watch_and_order_many(self, symbol, money, watch_percent):
+    def watch_and_order_many(self, f, symbol, money, watch_percent):
         infos = self.upbit.fetch_ticker(symbol)
         # print(infos)      # 해당 symbol의 정보 출력 (symbol, 고가, 현재가, 저가, 시작가 등등등)
         buy_sell    = "buy"
@@ -26,16 +34,21 @@ class Myupbit():
         percent     = infos["percentage"] * 100     # 전일 종가대비 현재가 % 차이
         rounded_percent = round(percent, 2)         # 전일 종가대비 현재가 % 차이 (소숫점 2자리 반올림)
         
-        if percent < watch_percent and self.is_buy_count < 10:
+        if percent < watch_percent and self.is_buy_count < 100:             # 원하는 등락폭과 충 매수/매도 100번까지만 하도록 제한함
             self.is_buy_count += 1  # 매수/매도 건수 증가
             # self.upbit.create_market_order(symbol, "buy", money, 1)       # 가상화폐를 구입하라
             # print(rounded_percent)    
             if rounded_percent < 0 : 
-                rounded_percent_text = "떨어져서"
+                rounded_percent_text = "Down"
             else:
-                rounded_percent_text = "올라서"
+                rounded_percent_text = "UP"
             
-            print(f'{buy_sell} - {date_time} {symbol}의 전일종가 {preprice}원, 현재가 {price}, 전일대비 {rounded_percent}% {rounded_percent_text}, {money}원 주문')
+            log_txt = f'{buy_sell},{date_time},{symbol},{preprice},{price},{rounded_percent}%,{rounded_percent_text},{money}'
+            f.write(log_txt + "\n")
+            #print(f"'{:03d}'.self.is_buy_count번 - {log_txt}")
+            print(f"{self.is_buy_count:03d} - {log_txt}")
+
+            # print(f'{buy_sell} - {date_time} {symbol}의 전일종가 {preprice}원, 현재가 {price}, 전일대비 {rounded_percent}% {rounded_percent_text}, {money}원 주문')
             #result_text = pd.DataFrame([[date_time, buy_sell, symbol, preprice, price, rounded_percent, rounded_percent_text, money]], columns=["date_time", "buy_sell", "symbol", "preprice", "price", "rounded_percent", "rounded_percent_text", "money"], index=["0"])
             #print(result_text)
                   
